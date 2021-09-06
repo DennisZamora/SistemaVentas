a960f4734b23dd7b66009c36bcaec346b31081fd
DennisZamora <78609499+DennisZamora@users.noreply.github.com>
2021-09-06 10:54:53 -0600

    Se crean los files y se agrega el archivo de la bd

---
+++ MySQL/bd.sql
@@ -0,0 +1,29 @@
+create database ventas;
+use ventas;
+
+create table alquiler(
+id int not null auto_increment,
+imagen longblob,
+ubicacion varchar(100) not null,
+descripcion varchar(200) not null,
+precio double not null,
+PRIMARY KEY(ID)
+);
+
+create table ventas(
+idVentas int not null auto_increment,
+imagen longblob,
+ubicacion varchar(100) not null,
+descripcion varchar(200) not null,
+precio double not null,
+PRIMARY KEY(idVentas)
+);
+
+create table lotes(
+idLotes int not null auto_increment,
+imagen longblob,
+ubicacion varchar(100) not null,
+descripcion varchar(200) not null,
+precio double not null,
+PRIMARY KEY(idLote)
+);
